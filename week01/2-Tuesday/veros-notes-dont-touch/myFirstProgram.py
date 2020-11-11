1e8e0b11fa69a184e03fde7235846a2d0877bfa4
Veronica Lino <veronica@digitalcrafts.com>
2020-11-11 09:00:44 -0600

    labs

---
+++ week01/2-Tuesday/veros-notes-dont-touch/myFirstProgram.py
@@ -0,0 +1,46 @@
+
+"hello world"
+
+# 'this is a great day!'
+
+
+"I\'m going to the store after \" \" \'\' class"
+
+
+"""
+Lorem Ipsum is simply dummy text
+    of the printing and typesetting industry.\v
+     
+Lorem Ipsum has been the industry's 
+standard dummy text ever since the 1500s, 
+when an unknown printer took a galley 
+of type and scrambled it to make a type 
+specimen book. It has survived
+not only five centuries, but also the leap 
+into electronic typesetting, remaining essentially unchanged. It was 
+popularised in the 1960s with the release of Letraset sheets containing 
+Lorem Ipsum passages, and more recently with desktop publishing software like 
+Aldus PageMaker including versions of Lorem Ipsum.
+
+"""
+
+"hello" + " world"  # hello world
+
+
+2 ** 2  # 4
+5 ** 3  # 125
+
+5 % 2  # 1
+
+7 % 2  # 1
+
+5 // 2  # 2
+
+name = "Jordan"
+
+age = 22
+
+
+"Hello" + name
+
+isValid = True
