2f43f68181d756966f654e5910c1afad5b35f3ae
Veronica Lino <veronica@digitalcrafts.com>
2020-11-12 09:55:33 -0600

    lists

---
+++ week01/3-Wednesday/veros-notes-dont-touch/if.py
@@ -0,0 +1,89 @@
+
+
+# if True:
+#     print("inside of if statement")
+
+    
+# print("secoond line of code inside of if statement")
+# print('hello world')
+
+# variable called age
+# age = 25
+
+# if age == 25 :
+#     print(age)
+    
+# name = "Veronica"
+
+# if (name  == 'Veronica'):
+#     print(name)
+    
+# print("outside of if statement")
+
+# if (condition):
+#     block 
+# else :
+#     block
+
+
+# age = 17
+
+# if( age > 21):
+#     print('you are old enough to drink')
+# elif(age >= 18 and age < 21):
+#     print('you\'re not quite old enough')   
+# else:
+#     print('what are you doing?  You can\'t drink')
+
+# age = 25
+
+# if( age > 21):
+#     print('you are old enough to drink')
+# elif(age < 21):
+#     print('you\'re not quite old enough')   
+
+
+# if( age > 21):
+#     print('you are old enough to drink')
+    
+# if( age > 21):
+#     print('you are old enough to drink')
+
+
+# elif(age >= 18 and age < 21):
+
+
+# count = 0
+
+# count = count + 1  # 1
+
+# count = count + 1 #  2
+
+# count = count + 1 #  3
+
+
+# count += 1
+
+# count = 0
+ 
+# while count < 10:
+#     count += 1
+#     print(f"The count is {count}")
+
+
+# print("done with count")
+
+# answer = ''
+
+# while answer != 'when':
+#     answer = input('Say when: ')
+#     answer = answer.lower()
+    
+    
+# print('cheese')
+
+x = 0
+
+x-= 3
+
+x = x - 3
---
+++ week01/3-Wednesday/veros-notes-dont-touch/python101.py
@@ -0,0 +1,149 @@
+
+# school = "Digital Crafts School"
+
+
+# print(school)
+# print(school)
+# print(school)
+# print(school)
+# print(school)
+# print("Digital Crafts School")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts School")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+# print("Digital Crafts")
+
+# student1 = "Andrea"
+# student2 = "Zach"
+# student3 = "Michael"
+# student4 = "Rick"
+
+# "Good Morning"
+
+# "".format()
+# formatting = "Good Morning: {1} {1} {1} {1}".format(
+#     student1, student2,  student3, student4)
+
+# print(formatting)
+
+
+# 0. Andrea
+# 1. Zach
+# 2. Michael
+# 3. Rick
+
+# formatting = "Good Morning: {Andrea} {Zach}".format(
+#     Andrea=student1, Zach=student2)
+
+# print(formatting)
+
+
+# f'strings
+
+# day = "Wednesday"
+# tomorrow = "Thursday"
+
+# currentDay = f"Today is {day}"
+# currentDay1 = f"Tommorrow is {tomorrow}"
+
+
+# # print(f"Today is {day}")
+# print(currentDay)
+# print(currentDay1)
+
+# type("hello world")
+
+# print(type("Hello world"))
+# print(type(5))
+# print(type(5.1))
+# print(type(True))
+
+# print(type())
+
+# a = "hello"
+# b = 5
+# c = True
+# d = 5.6
+
+# print(isinstance(a, int))
+# print(isinstance(a, str))
+# print(isinstance(c, int))  # int will be returned for bool statements
+# print(isinstance(c, float))
+
+# a_string = '1'  # string
+# a = '2'  # string
+# an_int = 2
+
+# c = "hello"
+# d = " world"
+
+# e = 1
+# f = 2
+
+# print( e + f)
+# print( a_string + an_int)
+
+# str + int
+
+# print(a_string + a)
+
+# print(c + d)
+
+# print("hello world")
+
+# one = "one1"
+# two = "two1"
+# three = "three1"
+
+# print(one, two, three)
+
+# print(one)
+# print(two)
+# print(three)
+
+
+# print("""
+# \tVestibulum in ornare tellus, nec porttitor risus.
+# Aenean eget  \b\b\bfacilisis massa, quis consequat justo.\v
+# Vestibulum laoreet gravida fermentum. Etiam nulla\n metus, pellentesque id magna sit amet, porta bibendum lectus. Vivamus malesuada congue lorem vitae facilisis. Vivamus risus felis, scelerisque sed porttitor a, viverra nec purus. Duis et placerat felis. In sed diam ac orci molestie malesuada. Cras aliquet, turpis ac aliquam consequat, leo elit ultrices diam, non hendrerit orci massa placerat nisi. Suspendisse malesuada neque in varius sagittis. Fusce at porttitor enim. Sed accumsan lorem id tempor convallis. Donec tristique interdum lorem, eget interdum urna porta viverra. Vestibulum laoreet orci massa, at pretium nisl lobortis a. Curabitur sit amet est semper libero ultrices fermentum at non arcu.
+# """)
+
+
+# one_str = "hello"
+
+# two = 2
+
+# one = int(one_str)
+
+# print(one + two)
+
+# one_int = 1
+
+# one_str = str(one_int)
+
+# print("my number is " + one_str)
+
+
+# print(f"my number is {one_str}")
+
+# print("my number is {}".format(one_str))
+
+# name = input("What is your name?")
+
+# age = input("how old are you")
+
+
+# yearOfBirth = 2020 - int(age)
+
+# print(f"my user entered in {name}")
+# print(f"year of birth {yearOfBirth}")
+# print(type(name))
+
+# print(5 > 4)
