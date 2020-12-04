7a4808d8aded68f460c9de02b16e02d28415c102
Veronica Lino <veronica@digitalcrafts.com>
2020-12-04 12:55:39 -0600

    corona

--- week04/5-Friday/veros-notes-dont-touch/labs/dom/js/js/get-element-by-id.js
+++ week04/5-Friday/veros-notes-dont-touch/labs/dom/js/js/get-element-by-id.js
@@ -1,4 +1,8 @@
 // Select the element and store it in a variable.
 
+document.getElementById('one');
+document.getElementById('two');
+document.getElementById('three');
+document.getElementById('four');
 
 // Change the value of the class attribute.
