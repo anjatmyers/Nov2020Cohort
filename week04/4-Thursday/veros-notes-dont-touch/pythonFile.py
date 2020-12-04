39baa6c4adddc9204626f6a2311ce13c3ae06982
Veronica Lino <veronica@digitalcrafts.com>
2020-12-04 09:23:34 -0600

    dom

---
+++ week04/4-Thursday/veros-notes-dont-touch/pythonFile.py
@@ -0,0 +1,46 @@
+print('hello world')
+
+
+a = 3
+
+digital_crafts = "school"
+
+
+# len(digital_crafts)
+
+# boolean = True
+
+# b = "c"
+
+# a + b
+
+# True False
+
+# if a not b
+
+# and
+
+# or
+
+# if condition:
+#     block
+# elif condition:
+#     block
+# else :
+#     block
+
+
+# while (condition):
+#     #block
+
+
+ arr = [6, 7,8, 9, 9]
+
+# arr.append(5)
+
+# a = '7'
+# int(a)
+
+
+for val in arr:
+    print(val)
