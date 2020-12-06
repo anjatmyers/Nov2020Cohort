9d23c9f5c4c9482e775e3319c064e945df0f96c1
Andrea Myers <andreatmyers5@gmail.com>
2020-12-06 13:25:14 -0500

    Sunday hw

--- week04/5-Friday/labs/dom/js/get-element-by-id.js
+++ week04/5-Friday/labs/dom/js/get-element-by-id.js
@@ -1,4 +1,40 @@
 // Select the element and store it in a variable.
 
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
+
 
 // Change the value of the class attribute.
+
+// one.innerHTML = 'Changed'
+// this changed the text of the ID 'one' to 'changed 
+
+// innerHTML lets you put HTML to style the text
+// innerText only lets you put text 
+
+// getElementsByClassName returns an array because it could be multiple values
+
+// var arrHotClass = document.getElementsByClassName('hot');
+
+// i = 0
+// while (i < arrHotClass.length){
+//     arrHotClass[i].textContent = "What up boo??";
+//     i++;
+// }
+
+// arrHotClass[1].textContent = "HEYYYY"
+
+// var listItems = document.getElementsByTagName('li');
+
+// for( var i = 0; i < listItems.length; i++) {
+//     listItems[i].textContent = "Oooo okayyy. You a badie"; }
+
+// textContent
+// innerHTML
+// innerText
+
+
+
+
