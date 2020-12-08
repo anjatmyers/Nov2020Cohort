864917fbacae78f11172cdbecd1b1d434d85abd3
Andrea Myers <andreatmyers5@gmail.com>
2020-12-08 11:55:05 -0500

    Monday after class

--- week04/5-Friday/labs/corona/js/coronaLayout.js
+++ week04/5-Friday/labs/corona/js/coronaLayout.js
@@ -1,6 +1,4 @@
 
-function stateStats() {
-    for(var i=0; i < data.length; i++){
 
     
 // find body and assign to a variable 
@@ -15,6 +13,13 @@ body.appendChild(divContainer1);
 var divRow2 = document.createElement('div');
 divRow2.setAttribute('class', 'row');
 divContainer1.appendChild(divRow2);
+
+// append all cards to row above 
+
+// create function to iterate through state data
+
+function stateStats() {
+    for(var i=0; i < data.length; i++){
 
 // create div column
 var divCol3 = document.createElement('div');
@@ -39,19 +44,43 @@ cardTitle.textContent = `${data[i].state}`
 divCardBody5.appendChild(cardTitle);
 
 // create card subtitle h6 heading appended to divCardBody 5
-var cardSubTitle = document.createElement('h5');
-cardSubTitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
-cardSubTitle.textContent = `Total Cases to date: ${data[i].cases}`
-divCardBody5.appendChild(cardSubTitle);
+var cardSubTitle1 = document.createElement('h6');
+cardSubTitle1.setAttribute('class', 'card-subtitle mb-2 text-muted');
+cardSubTitle1.textContent = `Total Cases to date: ${data[i].cases}`
+divCardBody5.appendChild(cardSubTitle1);
 
-// create paragraph appended to divCardBody 5
-var cardText = document.createElement('h6');
-cardText.setAttribute('class', 'card-text');
-cardText.textContent = `Deaths per million: ${data[i].deathsPerOneMillion}`
-divCardBody5.appendChild(cardText);
+// create second card subtitle h6 heading appended to divCardBody 5
+var cardSubTitle2 = document.createElement('h6');
+cardSubTitle2.setAttribute('class', 'card-subtitle mb-2 text-muted');
+cardSubTitle2.textContent = `Deaths to date: ${data[i].deaths}`
+divCardBody5.appendChild(cardSubTitle2);
+
 
     }
-
 }
 
 stateStats ();
+
+
+
+// Veronica's solution ...........................
+
+// var html = "";
+// data.forEach(function(obj){
+//     html += `
+//     <div class="col-3 mt-5 mr-3">
+//         <div class="card" style="width: 18rem;">
+//             <div class="card-body">
+//                 <h5 class="card-title">${obj.state}</h5>
+//                 <h6 class="card-subtitle mb-2 text-muted">Cases: ${obj.cases}</h6>
+//                 <h6 class="card-subtitle mb-2 text-muted">Deaths: ${obj.deaths}</h6>
+//             </div>
+//             </div>
+//     </div><!--end of col-->
+//     `
+// })
+// // console.log(html)
+// //find the div with row class 
+// var row = document.querySelector('.row');
+// row.innerHTML = html;
+// // .innerHTML
--- week05/1-Monday/labs/js/companies.js
+++ week05/1-Monday/labs/js/companies.js
@@ -13,17 +13,75 @@ const companies = [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
+// var arr = [6, 4, 3, 2, 1];
+
+// var newArr = arr.map(function(num) {
+
+//     return num * 3;
+// })
+
+// console.log(newArr);
+
+
+// var students = ['Andrea', 'Kim', 'Kanny', 'Ally']
+
+// var greetingArr = students.map(function(student){
+
+//         return `Hello ${student}`
+// })
+
+// console.log(greetingArr);
+
+// .............................................
+
+
+// ages.forEach(function(age){
+//     let birthYear = 2020 - age;
+//     console.log(birthYear);
+
+// })
+
+
 // Filter out all ages greater than 35 
 // var ageFilter = ages.filter((age)=> age<=35)
+
+// var over35 = ages.filter(function(num){
+
+//     return num > 35;
+// })
+
+// console.log(over35);
 
 
 // Filter all even numbers
 
+// var evenNum = ages.filter(function(num){
+
+//     if (num % 2 === 0){
+//         return num
+//     }
+
+// })
+
+// console.log(evenNum);
 
 /// map through ages array and return a new ages array where 5 is added to each element.
 
 
+// var agesPlusFive = ages.map(function(num){
+//      return num + 5;
+    
+// })
+// console.log(ages);
+// console.log(agesPlusFive);
+
 // var newAges = ages.map(arrVAl => arrVAl +5)
+
+
+
+
+
+
 //map through the companies array and change the end date to 2020
 
 
@@ -33,3 +91,43 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //using forEach print each object of companies array
 
 
+// check if there is a number greater than 60 in the ages array 
+
+// var over60 = ages.some(function(num){
+//     return num > 60
+// })
+// console.log(over60);
+
+
+
+
+//map through the companies array and list the name, start and end data
+
+// companies.forEach(function(object){
+    
+//     console.log(object.name, object.start, object.end);
+
+// })
+
+// return an array with just company names
+// var comNames = companies.map(function(object){
+//     return object.name;
+// })
+
+// console.log(comNames);
+
+// 
+
+
+// filter out companies started before 1990
+
+// var start1Before1990 = companies.filter(function(object){
+
+//     return object.start >= 1990;
+//     // if (object.start > 1990) {
+//     //     return object.name
+//     // }
+    
+// })
+
+// console.log(start1Before1990);
